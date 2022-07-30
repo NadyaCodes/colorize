ffb45905ee43e52e3b60c08e3ef024cb783db9ab
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-30 09:32:42 -0400

    connect initial pug page

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const path = require('path')
 const app = express();
 const port = process.env.PORT || "8000";
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug")
+
 app.get("/", (req, res) => {
-  res.status(200).send("WHATABYTE: Food For Devs")
+  res.render("index", {title: "Home"});
 })
+
 
 
 app.listen(port, () => {
