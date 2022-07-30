5c3a145e42885a322c996ccc59c5ff5083e5abd1
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-30 09:52:16 -0400

    add .env

---
+++ bs-config.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+module.exports = {
+  proxy: `localhost:${process.env.PORT}`,
+  files: ["**/*.css", "**/*.pug", "**/*.js"],
+  ignore: ["node_modules"],
+  reloadDelay: 10,
+  ui: false,
+  notify: false,
+  port: process.env.PORTBROWS,
+};
